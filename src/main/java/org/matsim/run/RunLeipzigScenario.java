b449dcb40f7752ff888ed4b223087ac2a983a8f7
Benjamin Gabber <benjamin.gabber@mailbox.tu-dresden.de>
2023-07-29 11:41:49 +0200

    AreaBasedToll

--- src/main/java/org/matsim/run/RunLeipzigScenario.java
+++ src/main/java/org/matsim/run/RunLeipzigScenario.java
@@ -118,9 +118,9 @@ import static org.matsim.contrib.roadpricing.RoadPricingUtils.*;
 })
 public class RunLeipzigScenario extends MATSimApplication {
 
-	private Boolean useRoadPricing = false;
+	private Boolean useRoadPricing = true;
 
-	private Boolean useCarEnterPenalty = false;
+	private Boolean useCarEnterPenalty = true;
 
 	private static final Logger log = LogManager.getLogger(RunLeipzigScenario.class);
 
@@ -451,7 +451,7 @@ public class RunLeipzigScenario extends MATSimApplication {
 
 		for (Link link : scenario.getNetwork().getLinks().values()){
 
-			double tollCost = link.getLength() * 0.054 / 1000; // initTollPricing based on distance
+			double tollCost = 0;
 
 			//if (link.getFreespeed() <= 30/3.6){
 			//	addLinkSpecificCost( scheme, link.getId(), Time.parseTimeToSeconds("00:00:00"), Time.parseTimeToSeconds("24:00:00"), 10 );
@@ -462,6 +462,7 @@ public class RunLeipzigScenario extends MATSimApplication {
 			Boolean inEducation = tollEducationZone.checkIfLinkInFeature(link, "1");
 
 			if (link.getFreespeed() <= 30/3.6) {
+				tollCost = link.getLength() * 0.054 / 1000; // initTollPricing based on distance
 				if (inLowEmission) {
 					tollCost *= 1.2;
 					if (inResidential) {
@@ -473,7 +474,7 @@ public class RunLeipzigScenario extends MATSimApplication {
 				}
 			}
 
-			addLinkSpecificCost(scheme, link.getId(), Time.parseTimeToSeconds("00:00:00"), Time.parseTimeToSeconds("24:00:00"), tollCost);
+			addLinkSpecificCost(scheme, link.getId(), Time.parseTimeToSeconds("00:00:00"), Time.parseTimeToSeconds("48:00:00"), tollCost);
 
 		}
 		/* Add the link-specific toll. */
